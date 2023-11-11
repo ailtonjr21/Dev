ad17de1e7e840d2bc0d5a3fcbb9756b2b4a4c42f
Ailton Junior <ailtonjr21@gmail.com>
2023-11-11 08:55:52 -0300

    Consumindo API e Criando Interfaces

---
+++ TypeScript/app/src/services/negociacoes-service.ts
@@ -0,0 +1,17 @@
+import { NegociacaoDoDia } from "../interfaces/negociacao-do-dia.js";
+import { Negociacao } from "../models/negociacao.js";
+
+export class NegociacoesService{
+
+    public obterNegocicoesDoDia():Promise<Array<Negociacao>>{
+        return fetch('http://localhost:8080/dados') 
+            .then(res=> res.json())
+            .then((dados:Array<NegociacaoDoDia>)=>{
+                return dados.map(dado =>{
+                    return new Negociacao(new Date(), 
+                            dado.vezes, 
+                            dado.montante)
+                })
+            })
+    }
+}
